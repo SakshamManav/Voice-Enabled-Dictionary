ca121988cb256a6eb57eac60ac3c283a7ed7cc5e
SakshamManav <manavsaksham06@gmail.com>
2024-03-12 21:41:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let url = " https://api.dictionaryapi.dev/api/v2/entries/en/";
+let input = document.querySelector("#input");
+let btn = document.querySelector(".btn");
+let word1 = document.querySelector(".word");
+let sentence = document.querySelector(".sentence");
+let word;
+
+
+async function dictionary(word) {
+  word = input.value;
+  let data = await fetch(url + word);
+  var response = await data.json();
+
+  console.log(response);
+  if (data.ok) {
+    word1.innerHTML = response[0].meanings[0].definitions[0].definition;
+    word1.style.color = "black";
+  } else if(!data.ok) {
+    word1.innerHTML = "Could not find the word";
+    word1.style.color = "Red";
+    console.log("not ok");
+  }
+}
+
+
+btn.onclick = () => {
+  console.log("clicked");
+  dictionary(word);
+};
